d3f7c2be1c32ab7fc8952e5f548d9f0ae5e74354
carolynm2003 <carolyn.n.mario@student.uts.edu.au>
2024-05-18 19:05:59 +1000

    Add files via upload

---
+++ NutriFlex/NutriFlex/Calculator/InfoSheetView.swift
@@ -0,0 +1,63 @@
+//
+//  InfoSheetView.swift
+//  NutriFlex
+//
+//  Created by Carolyn Mario on 11/5/2024.
+//
+
+import SwiftUI
+
+struct InfoSheetView: View {
+    
+    @Environment(\.presentationMode) var presentationMode
+    
+    var body: some View {
+        ZStack {
+            Color(.systemBrown).opacity(0.5)
+                .ignoresSafeArea()
+            VStack{
+                Spacer()
+                Text("Use the calorie calculator to estimate the number of daily calories your body needs to maintain, loss or gain weight.")
+                    .font(.headline)
+                    .padding()
+                
+                Text("Note:  If you're pregnant or breast-feeding, are a competitive athlete, or have a metabolic disease, such as diabetes, the calorie calculator may overestimate or underestimate your actual calorie needs.")
+                    .font(.headline)
+                    .padding()
+                VStack(alignment:.leading,spacing: 10){
+                    Text(" ⦿ Minimal Activity (little or no exercise)")
+                    Text(" ⦿ Lightly active (exercise 1–3 days/week)")
+                    Text(" ⦿ Moderately active (exercise 3–5 days/week)")
+                    Text(" ⦿ Active (exercise 6–7 days/week)")
+                    Text(" ⦿ Very active (hard exercise 6–7 days/week)")
+                }
+                Spacer()
+            }
+            .padding()
+        .overlay(backButton , alignment: .topLeading)
+        }
+    }
+}
+
+struct InfoSheetView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoSheetView()
+    }
+}
+
+extension InfoSheetView{
+    private var backButton:some View{
+        Button{
+            presentationMode.wrappedValue.dismiss()
+        } label:{
+            Image(systemName: "xmark")
+                .font(.headline)
+                .padding(16)
+                .foregroundColor(.primary)
+                .background(.ultraThickMaterial)
+                .cornerRadius(10)
+                .shadow(radius: 4)
+                .padding()
+    }
+}
+}
